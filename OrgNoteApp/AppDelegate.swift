f4c8f0d1cc79f7e9448d5f960a432315b8ddeeb4
vkandel <vijaya.prakash.kandel@zalando.de>
2018-05-13 12:00:44 +0200

    Added barebones orgnote project

---
+++ OrgNoteApp/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  OrgNoteApp
+//
+//  Created by Vijaya Prakash Kandel on 13.05.18.
+//  Copyright © 2018 com.kandelvijaya. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ OrgNoteApp/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  OrgNoteApp
+//
+//  Created by Vijaya Prakash Kandel on 13.05.18.
+//  Copyright © 2018 com.kandelvijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+}
+
