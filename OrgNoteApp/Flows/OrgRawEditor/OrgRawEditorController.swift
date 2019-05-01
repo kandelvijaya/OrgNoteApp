9e977c7788d8c0fd5dbb749629e8fd45952883ce
vkandel <vijaya.prakash.kandel@zalando.de>
2019-05-01 17:47:18 +0200

    Added keyboard and a done button to dismiss keyboard as shortcut

--- OrgNoteApp/Flows/OrgRawEditor/OrgRawEditorController.swift
+++ OrgNoteApp/Flows/OrgRawEditor/OrgRawEditorController.swift
@@ -14,7 +14,8 @@ final class OrgRawEditorController: UIViewController {
     private var orgFile: OrgFile!
     private var onDismiss: ((OrgFile?) -> Void)!
     @IBOutlet weak var textView: UITextView!
-
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
     static func create(input orgFile: OrgFile, onDismiss: @escaping (OrgFile?) -> Void) -> OrgRawEditorController {
         let vc = UIStoryboard(name: String(describing: OrgRawEditorController.self), bundle: Bundle(for: OrgRawEditorController.self)).instantiateViewController(withIdentifier: String(describing: OrgRawEditorController.self)) as! OrgRawEditorController
         vc.orgFile = orgFile
@@ -27,6 +28,54 @@ final class OrgRawEditorController: UIViewController {
         customizeTextView()
         setOrgFileInTextView()
         textView.delegate = self
+        setupKeyboardNotification()
+        setupKeyboardShortcuts()
+    }
+    
+    private func setupKeyboardShortcuts() {
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            let dismissItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(hideKeyboard(_:)))
+            let dismissGroup = UIBarButtonItemGroup(barButtonItems: [dismissItem], representativeItem: nil)
+            textView.inputAssistantItem.leadingBarButtonGroups = [dismissGroup]
+        } else {
+            let buttonToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 40))
+            let spaceItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+            let buttonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(hideKeyboard(_:)))
+            buttonToolbar.items = [spaceItem, buttonItem]
+            textView.inputAccessoryView = buttonToolbar
+        }
+    }
+    
+    @objc private func hideKeyboard(_ item: UIBarButtonItem) {
+        self.textView.resignFirstResponder()
+    }
+    
+    private func setupKeyboardNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let keyboardAnimationTime: Double = notification.userInfo?["UIKeyboardAnimationDurationUserInfoKey"] as? Double,
+            let keyboardEndFrame: CGRect = notification.userInfo?["UIKeyboardFrameEndUserInfoKey"] as? CGRect,
+            let keyboardBeginFrame: CGRect = notification.userInfo?["UIKeyboardFrameBeginUserInfoKey"] as? CGRect else {
+                return
+        }
+        let verticalDistance: CGFloat = keyboardBeginFrame.origin.y - keyboardEndFrame.origin.y
+        let safeBottomInsetThatKeyboardDoesnotUse: CGFloat = UIApplication.shared.keyWindow?.safeAreaInsets.bottom ?? 0.0
+        let applicableVerticalDistance: CGFloat = verticalDistance - safeBottomInsetThatKeyboardDoesnotUse
+        UIView.animate(withDuration: keyboardAnimationTime) {
+            self.bottomConstraint.constant = -applicableVerticalDistance
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        guard let keyboardAnimationTime: Double = notification.userInfo?["UIKeyboardAnimationDurationUserInfoKey"] as? Double else { return }
+        UIView.animate(withDuration: keyboardAnimationTime) {
+            self.bottomConstraint.constant = 0
+            self.view.layoutIfNeeded()
+        }
     }
 
     private func customizeTextView() {
